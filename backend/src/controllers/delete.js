19966f4f446152fb86c869fc233363a06a4f4af5
Armando <a.guerra58@hotmail.com>
2022-02-16 22:55:20 -0300

    mudando controller delete

--- backend/src/controllers/delete.js
+++ backend/src/controllers/delete.js
@@ -3,11 +3,6 @@ const deleteTask = require('../models/tasks/delete');
 const deleteTaskById = async (request, response) => {
   const { id } = request.params;
   const taskDeleted = await deleteTask(id);
-  if (taskDeleted.err) {
-    return response
-      .status(422)
-      .json({ err: { code: taskDeleted.err.code, message: taskDeleted.err.message } });
-  }
   return response.status(200).json(taskDeleted);
 };
 
